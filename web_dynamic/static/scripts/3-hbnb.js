9916fbaa776abdacd5922c1543085229c51ee8f9
Moonwalkert3ch <6620@holbertonstudents.com>
2023-12-11 20:42:52 -0600

    creating places

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -19,4 +19,21 @@ $(document).ready(() => {
         $('DIV#api_status').removeClass('available');
     }
   });
+
+  const getPlaces = {
+    $.ajax({
+        url: "http://0.0.0.0:5001/api/v1/places_search/",
+        type: "POST",
+        data: "{}",
+        dataType: "json",
+        contentType: "application/json",
+        success: function (data) {
+            $(".places").empty();
+            for (let elem of data) {
+                //appends elem for each place
+                $(".places").append('<article><div class="title"><h2>' + elem['name'] + '</h2> <div clas="price_by_night">' + elem['price_by_night'] + '</div></div> <div class="information"><div class="max_guest">')
+            }
+        }
+    })
+  }
 });
